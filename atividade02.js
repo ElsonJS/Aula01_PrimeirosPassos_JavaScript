159b95b2bc8116cb5698b15870099b452b3e2f16
Elson Souza <107326097+ElsonJS@users.noreply.github.com>
2022-07-28 18:45:23 -0300

    Update and rename atividade02.js to atividade02.html

--- atividade02.js
+++
@@ -1,7 +0,0 @@
-<script>
-    //Faça um Programa que peça um número e então mostre a mensagem "O número informado foi [número]"
-    var n1 = prompt("Digite um numero: ");
-    alert("O número informado foi: "+ n1);
-
-</script>
-
