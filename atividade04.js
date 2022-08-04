53b76b07e77286a8726cd2ebbc8f07b388253f28
Elson Souza <107326097+ElsonJS@users.noreply.github.com>
2022-08-04 20:16:45 -0300

    Update and rename atividade04.html to atividade04.js

---
+++ atividade04.js
@@ -0,0 +1,10 @@
+<script>
+     // Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+     var n1=prompt("Digite a 1° Nota: ");
+     var n2=prompt("Digite a 2° Nota: ");
+     var n3=prompt("Digite a 3° Nota: ");
+     var n4=prompt("Digite a 4° Nota: ");
+      
+     var soma=parseInt(n1)+parseInt(n2)+parseInt(n3)+parseInt(n4);
+     alert("Sua média das notas é: "+ soma/4);
+</script>
