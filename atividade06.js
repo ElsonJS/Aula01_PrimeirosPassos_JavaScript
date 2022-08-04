c73f6a809f45215994fd2ed1e40995f219a87b13
Elson Souza <107326097+ElsonJS@users.noreply.github.com>
2022-08-04 20:18:24 -0300

    Update and rename atividade06.html to atividade06.js

---
+++ atividade06.js
@@ -0,0 +1,9 @@
+<script>
+     /*Faça um Programa que pergunte quanto você ganha por hora e o número de
+     horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.*/
+     var n1=prompt("Quanto você recebe por hora? ");
+     var n2=prompt("Quantas horas você trabalha por mês? ");
+      
+     var soma=parseInt(n1);
+     document.write("<h2>Seu salário do mês: "+  soma*n2 + "</h2>");
+</script>
