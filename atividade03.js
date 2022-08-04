76f5d16cc44db48b7bee94cc65d0865f08daadd7
Elson Souza <107326097+ElsonJS@users.noreply.github.com>
2022-08-04 20:15:59 -0300

    Update and rename atividade03.html to atividade03.js

---
+++ atividade03.js
@@ -0,0 +1,8 @@
+<script>
+     //Faça um Programa que peça dois números e imprima a soma.
+     var n1=prompt("Digite o numero 1°: ");
+     var n2=prompt("Digite o numero 2°: ");
+     var soma=parseInt(n1)+parseInt(n2);
+    
+     alert("A soma dos numeros digitados é: "+ soma);
+</script>
