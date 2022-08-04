16754260b0038c17095d1a4906447f59e8d62566
Elson Souza <107326097+ElsonJS@users.noreply.github.com>
2022-08-04 20:14:32 -0300

    Update and rename atividade01.html to atividade01.js

---
+++ atividade01.js
@@ -0,0 +1,6 @@
+<script>
+    //Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+    alert("Alo, mundo!!");              
+               
+    //document.write("<h2>Alo, mundo!!</h2>");
+</script>
